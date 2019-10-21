325b68d88180e49b60d6468f30761b5f17ad8848
huidadao <huidadao@gmail.com>
2019-10-21 21:21:44 +0900

    Updates

---
+++ precache-manifest.5b7fa7db6ead6762be0031560f8f9664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a61417a579d749db88f46a2ebf2a864",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "fbc137b3ecc289063d24",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1a47d578e1b1f92f7734",
+    "url": "/contactmanager/static/css/main.fe63296e.chunk.css"
+  },
+  {
+    "revision": "fbc137b3ecc289063d24",
+    "url": "/contactmanager/static/js/2.5cd320d0.chunk.js"
+  },
+  {
+    "revision": "1a47d578e1b1f92f7734",
+    "url": "/contactmanager/static/js/main.48612be2.chunk.js"
+  },
+  {
+    "revision": "52a8038624ffe69f6d88",
+    "url": "/contactmanager/static/js/runtime-main.9acb0f81.js"
+  }
+]);
